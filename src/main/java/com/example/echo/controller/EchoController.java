9cba02821accf24b24a0dd969ca2ea3f4a1402cf
Sergey Siroshtan <sergey.siroshtan@signanthealth.com>
2023-03-20 17:43:37 +0100

    another commit

--- src/main/java/com/example/echo/controller/EchoController.java
+++ src/main/java/com/example/echo/controller/EchoController.java
@@ -5,22 +5,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 @RestController
 @Slf4j
 public class EchoController {
 
     @GetMapping("/echo")
-    @CrossOrigin(origins = "*")
     public ResponseEntity<Object> get(@RequestBody(required = false) Object requestDto) {
         log.info("Enter GET /echo endpoint");
         return new ResponseEntity<>(requestDto, HttpStatus.OK);
     }
 
     @PutMapping("/echo")
-    @CrossOrigin(origins = "*")
     public ResponseEntity<Object> put(@RequestBody(required = false) Object requestDto) {
         log.info("Enter PUT /echo endpoint");
         return new ResponseEntity<>(requestDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/ip")
+    public ResponseEntity<String> getIp() throws UnknownHostException {
+        log.info("Enter GET /ip endpoint");
+        return new ResponseEntity<>(InetAddress.getLocalHost().getHostAddress(), HttpStatus.OK);
     }
 }
