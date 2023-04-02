89cebe20f6c7cf1b76d5e69863236cca07dfb5f1
Sergey Siroshtan <sergey.siroshtan@signanthealth.com>
2023-04-02 09:49:28 +0200

    another commit

--- src/main/java/com/example/echo/controller/EchoController.java
+++ src/main/java/com/example/echo/controller/EchoController.java
@@ -1,9 +1,12 @@
 package com.example.echo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -12,6 +15,10 @@ import java.net.UnknownHostException;
 @RestController
 @Slf4j
 public class EchoController {
+
+    private final WebClient client = WebClient.builder()
+            .baseUrl("http://localhost:8080")
+            .build();
 
     @GetMapping("/echo")
     public ResponseEntity<Object> get(@RequestBody(required = false) Object requestDto) {
@@ -29,5 +36,17 @@ public class EchoController {
     public ResponseEntity<String> getIp() throws UnknownHostException {
         log.info("Enter GET /ip endpoint");
         return new ResponseEntity<>(InetAddress.getLocalHost().getHostAddress(), HttpStatus.OK);
+    }
+
+    @PostMapping("/pass")
+    public ResponseEntity<String> pass() {
+        log.info("Enter POST /pass endpoint");
+        String response = client.get()
+                .uri("/ip")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        log.info("Response: " + response);
+        return new ResponseEntity<>("Passed", HttpStatus.OK);
     }
 }
